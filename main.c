852bb95dbf58d9f6110082a04948224559b23939
rajah <pierre.tonthat@free.fr>
2025-06-01 01:52:00 +0200

    add loops count

--- main.c
+++ main.c
@@ -37,6 +37,27 @@ GifFileType * CDECL gifenc_open(const char *fileName, int width, int height, int
   return gif;
 }
 
+int32_t CDECL gifenc_set_loops(GifFileType *gif, int loops)
+{
+  if (loops > 0 && loops <= 0xFFFF)
+  {
+    unsigned char netscape[12] = "NETSCAPE2.0";
+            
+    if (GifAddExtensionBlock(&gif->ExtensionBlockCount, &gif->ExtensionBlocks, APPLICATION_EXT_FUNC_CODE, 11, netscape) == GIF_OK)
+    {
+      unsigned char data[3];
+      
+      data[0] = 0x01;
+      data[1] = loops & 0xFF;
+      data[2] = (loops >> 8) & 0xFF;
+            
+      return GifAddExtensionBlock(&gif->ExtensionBlockCount, &gif->ExtensionBlocks, CONTINUE_EXT_FUNC_CODE, 3, data);
+    }
+  }
+  
+  return GIF_ERROR;
+}
+
 int32_t CDECL gifenc_add_image(GifFileType *gif, int left, int top, int width, int height, int colors, GifColorType* palette, uint8_t* chunky)
 {
   SavedImage frm = {0};
@@ -75,6 +96,7 @@ int32_t CDECL gifenc_set_special(GifFileType *gif, int frame_idx, int trnsprnt, 
   
   return GIF_ERROR;
 }
+
 int32_t CDECL gifenc_write(GifFileType *gif) { return (int32_t)EGifSpew(gif); }
 
 int32_t CDECL gifenc_close(GifFileType *gif) { return EGifCloseFile(gif, &error); }
@@ -89,7 +111,9 @@ PROC LibFunc[] =
   {"gifenc_get_lib_version", "const char* gifenc_get_lib_version();\n", gifenc_get_lib_version},
    
   {"gifenc_open", "GifFileType* gifenc_open(const char *fileName, int width, int height, int bckgrnd, int colors, uint8_t* palette);\n", gifenc_open},
-  
+	
+	{"gifenc_set_loops", "int32_t gifenc_set_loops(GifFileType *gif, int loops);\n", gifenc_set_loops},
+
   {"gifenc_add_image", "int32_t gifenc_add_image(GifFileType *gif, int left, int top, int width, int height, int colors, uint8_t* palette, uint8_t* chunky);\n", gifenc_add_image},
   {"gifenc_set_special", "int32_t gifenc_set_special(GifFileType *gif, int frame_idx, int trnsprnt, int disposal, int delay);\n", gifenc_set_special},
   
@@ -97,9 +121,9 @@ PROC LibFunc[] =
   {"gifenc_close", "int32_t gifenc_close(GifFileType *gif);\n", gifenc_close},
 
   {"gifenc_get_last_error", "const char* gifenc_get_last_error(GifFileType *gif);\n", gifenc_get_last_error},
-  };
+};
 
-LDGLIB LibLdg[] = { { 0x0001, 7, LibFunc, VERSION_LDG(GIFLIB_MAJOR, GIFLIB_MINOR, GIFLIB_RELEASE), 1} };
+LDGLIB LibLdg[] = { { 0x0001, 8, LibFunc, VERSION_LDG(GIFLIB_MAJOR, GIFLIB_MINOR, GIFLIB_RELEASE), 1} };
 
 /*  */
 
